22f5111976c1ed01fe5a8f27b512cf01a599bdf5
Troy <troyhen@churchofjesuschrist.org>
2021-04-29 08:54:36 -0600

    Add animated toolbar

--- app/src/main/java/com/troy/composeanimations/ui/Values.kt
+++ app/src/main/java/com/troy/composeanimations/ui/Values.kt
@@ -1,14 +1,32 @@
 package com.troy.composeanimations.ui
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -17,6 +35,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.troy.composeanimations.AppBar
@@ -28,10 +47,19 @@ fun ValueScreen(nav: Nav, onBack: () -> Unit) {
     BackHandler(onBack = onBack)
     Scaffold(topBar = { AppBar(nav, onBack) }) {
         Column(
-            Modifier
+            modifier = Modifier
                 .fillMaxWidth()
-                .padding(16.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+                .padding(top = 16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             ColorButton()
+            ToolBar(
+                Modifier
+                    .padding(top = 16.dp)
+                    .fillMaxWidth()
+                    .height(50.dp)
+                    .background(MaterialTheme.colors.surface)
+            )
         }
     }
 }
@@ -46,15 +74,6 @@ private fun ColorButton() {
     TextButton(onClick = { target = nextColor(target) }, colors = ButtonDefaults.textButtonColors(contentColor = target.contrast, backgroundColor = color)) {
         Text(target.title, fontSize = 20.sp)
     }
-}
-
-private fun nextColor(previous: ColorTarget): ColorTarget {
-    val values = ColorTarget.values()
-    var next = previous
-    while (next == previous) {
-        next = values[values.indices.random()]
-    }
-    return next
 }
 
 private enum class ColorTarget(val title: String, val color: Color, val contrast: Color = Color.Black) {
@@ -72,3 +91,57 @@ private enum class ColorTarget(val title: String, val color: Color, val contrast
     GREEN("Green", Color.Green),
 }
 
+private fun nextColor(previous: ColorTarget): ColorTarget {
+    val values = ColorTarget.values()
+    var next = previous
+    while (next == previous) {
+        next = values[values.indices.random()]
+    }
+    return next
+}
+
+private enum class Tool(val title: String, val imageVector: ImageVector) {
+    HOME("Home", Icons.Default.Home),
+    SEARCH("Search", Icons.Default.Search),
+    CART("Cart", Icons.Default.ShoppingCart),
+    ORDERS("Orders", Icons.Default.AccountBox),
+    SETTINGS("Settings", Icons.Default.Settings),
+}
+
+/**
+ * Render a toolbar with animated tool items when selected
+ */
+@Composable
+private fun ToolBar(modifier: Modifier = Modifier) {
+    var selected by remember { mutableStateOf(Tool.HOME) }
+    Row(modifier, verticalAlignment = Alignment.CenterVertically) {
+        Tool.values().forEach { tool ->
+            ToolItem(
+                tool = tool, selected = selected == tool,
+                Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+            ) { selected = tool }
+        }
+    }
+}
+
+/**
+ * Render a toolbar item which animates its size, color and label when selected
+ */
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+private fun ToolItem(tool: Tool, selected: Boolean, modifier: Modifier = Modifier, onClick: () -> Unit) {
+    Box(modifier.clickable { onClick() }, contentAlignment = Alignment.Center) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            val color = if (selected) MaterialTheme.colors.primary else Color.Gray
+            val size = if (selected) 30.dp else 24.dp
+            val animatedColor by animateColorAsState(color)
+            val animatedSize by animateDpAsState(size)
+            Icon(tool.imageVector, tool.title, Modifier.size(animatedSize), tint = animatedColor)
+            AnimatedVisibility(visible = selected) {
+                Text(tool.title, color = animatedColor, style = MaterialTheme.typography.caption)
+            }
+        }
+    }
+}
